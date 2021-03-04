bcc04b15e0d4f47f558b11de8b2970bf111f75b0
Leo34090 <leo.roussel@reseau.eseo.fr>
2021-03-04 14:43:22 +0100

    Create petite hisotire.py

---
+++ petite hisotire.py
@@ -0,0 +1,72 @@
+Tout au bout du chemin, Arthur distingue les gros cailloux sur un immense fond bleu et vert.
+Il s’élance le long du champ d’artichauts.
+Maman lui a acheté de nouvelles baskets, de celles qui galopent !
+Il est certain de pulvériser son propre record de vitesse pour atteindre le dolmen.
+Pas besoin de chronomètre, l’air salé n’a jamais autant mis ses cheveux en bataille.
+
+Le gosse est heureux.
+
+Il reprend son souffle en fin de course, s’adosse au mégalithe puis se laisse glisser pour s’asseoir.
+Face à l’immensité.
+Un imperceptible trait d’horizon sépare la mer du ciel.
+Ou inversement.
+Tête en haut ou tête en bas ?
+Peu importe, quand les vagues montent sur leurs grands chevaux, ciel et mer ne font qu’un.
+S’il avait des ailes, il pourrait s’élancer de cette falaise et voler avec les goélands.
+Ça, ce serait le pied !
+L’enfant balance ses jambes dans le vide, tout à sa rêverie.
+
+Il ressent soudain des vibrations. Une voix caverneuse s’élève :
+— Fais attention, Arthur. Si tu tombes, je ne pourrai pas te retenir !
+Intrigué, le gamin se lève lentement. Il caresse le granite et s’enquiert, hésitant :
+— C’est… toi qui as parlé ?
+Une faille sur le dessus du dolmen prend la forme d’une bouche et s’anime :
+— Mon nom est Eñvor. Je suis d’habitude muet comme une tombe, mais aujourd’hui tu es trop imprudent. Je devais intervenir.
+Interdit, Arthur ne sait que dire.
+— Promets-moi de ne plus t’approcher aussi près du précipice, c’est dangereux.
+Confus, Arthur baisse la tête. Il promet.
+
+À cet instant, un étrange personnage vient dissiper le malaise :
+— Allez, allez, ne faites pas ces têtes d’enterrement ! Il fait beau, les oiseaux chantent, haut les cœurs ! Voyons Eñvor, tu lui fais peur !
+Amusé, le jeune garçon voit une jolie poulette rousse, bien dodue, sortir du champ d’artichauts.
+— Qui es-tu ? demande-t-il.
+En guise de réponse, la nouvelle venue volète à destination de la table du dolmen et becque le front de l’enfant.
+— Aïe ! s’exclame Arthur en massant sa frange.
+— Je suis Honorine, la nettoyeuse, dit-elle, avant d’avaler un énorme ver de terre.
+Puis, de quelques battements de plumes désordonnés, elle saute à terre.
+Arthur rit de bon cœur en voyant la cocotte se dandiner devant lui.
+— Ne te moque pas petit, ou bien je te fais pousser une crête !
+— Pardon, prononce-t-il à voix basse en regardant le bout de ses magnifiques baskets.
+— Bien ! Passons aux choses sérieuses. Eñvor m’a confié que ton rêve est de voler.
+— Oui, c’est ça, avec les goélands ! Oh ! Il sait ça ?
+— Il n’y a pas que les murs qui ont des oreilles, les pierres aussi !
+— Eñvor a des oreilles ? Mais, je ne les ai pas vues !
+— C’est juste une expression, petit.
+Arthur reprend :
+— Comment peut-il savoir ? Je n’ai jamais rien dit à personne !
+— Justement, il a entendu tes pensées. Bon, si tu me disais plutôt comment tu comptes t’y prendre pour voler ?
+— Eh bien… Je vais tout simplement fabriquer des ailes !
+— Tu es complètement inconscient, mon ami. Laisse-moi te conter l’histoire d’Icare. C’est un jeune garçon qui a volé trop près du soleil avec des ailes que son père avait fabriquées. Il a voulu s’approcher de l’astre, subjugué par tant de beauté. Les ailes n’ont pas tenu à cause de la trop forte chaleur. Il est tombé puis est mort en mer.
+— Elle est triste ton histoire !
+— Oui, c’est triste. C’est pour ça que tu dois mettre toutes les chances de ton côté et réfléchir à la façon de concrétiser ton rêve. Tu as le temps, tu es encore jeune. Rentre chez toi maintenant, il se fait tard.
+Après un dernier regard vers Honorine et Eñvor, Arthur emprunte le chemin du retour.
+
+Jusqu’à la fin de l’été, le garçon est allé tous les jours au dolmen.
+Il n’a pas revu Honorine.
+Eñvor ne lui a plus parlé.
+
+Quinze ans après cette rencontre magique, Arthur est devenu un professionnel de parapente.
+
+Aujourd’hui est un jour particulier.
+Il est au dolmen.
+Il s’élance enfin de sa falaise.
+Un pincement au cœur, il voit les goélands.
+Ils volent à ses côtés.
+Il regarde en direction du mégalithe qui lui sourit.
+Il croit distinguer une poule.
+
+Arthur est heureux.
+Il est à nouveau ce gosse.
+À part que ce n’est plus un rêve.
+Il crie au ciel :
+— Je vole avec les goélands, c’est le pied !
